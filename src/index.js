6ac8652f99a5bd45e9a524a299a2e4cdb23985b8
fernandocieri <fernandocieri@gmail.com>
2021-12-26 14:26:06 +0100

    Started the bases of the component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
 import ReactDOM from 'react-dom';
-const html = "";
+
+/*function ListCreator() {
+  return (
+    <div id='addNewCard'>
+      <div className='cardTitle'>add a new list</div>
+      <button onClick={ }>+</button>
+    </div>
+  )
+}*/
+
+function List(props) {
+  const [listID, setListID] = useState(props.id);
+  const [listCards, setListCards] = useState(undefined);
+
+  async function getCards() {
+    /*get info of all cards for the list matching the id in the request. Stil not sure about the format of the info*/
+    const resp = await axios.get(`https://api.trello.com/1/lists/${listID}/cards`);
+    setListCards([...resp.text]);
+  }
+
+  return (
+    <div className='list'>
+      <Card /*fill props for card from mapping listCards. Check later*//>
+      <button>add card</button>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  html,
+  <List />,
   document.getElementById('root')
 );
-
