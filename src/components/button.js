8cf77a8eeadf8e0b215e3e08e54f623c1a201ef8
josemsb92 <josemsb92@gmail.com>
2022-01-04 12:33:21 +0100

    scss added

--- src/components/button.js
+++ src/components/button.js
@@ -4,8 +4,8 @@ import Button from '@mui/material/Button';
 
 export default function BasicButtons(props) {
   return (
-    <Stack spacing={2} direction="row">      
-      <Button variant="contained" className="editButton" onClick={props.onClick} >{props.value}</Button>      
+    <Stack spacing={2} direction="row" className={props.className} id="1">      
+      <Button variant="contained" onClick={props.onClick} >{props.value}</Button>      
     </Stack>
   );
 }
