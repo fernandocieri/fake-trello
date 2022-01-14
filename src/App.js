4599c9ccf256a1273079548b6ff74520f7a9befd
josemsb92 <josemsb92@gmail.com>
2022-01-14 19:49:28 +0100

    Link to "/" done on navbar "Texto provisional" button

--- src/App.js
+++ src/App.js
@@ -11,16 +11,14 @@ import {
 export default function App(){
     //IMPORTANT
     //Routing is made here.
-    return(
-        <div>
-        <Navbar/> {/* This component has a "title" prop to change the title of the component */}
+    return(               
         <Router>
+            <Navbar/> {/* This component has a "title" prop to change the title of the component */}
             <Routes>
                 <Route path="/" element={<WorkspaceContainer/>}/>
                 <Route path="activityspecs" element={<Activityspecs/>}/>
                 <Route path="board" element={<Board/>}/>                 
             </Routes>
-        </Router>
-        </div>
+        </Router>        
     )
 }
--- src/components/WorkspaceContainer.js
+++ src/components/WorkspaceContainer.js
@@ -1,18 +1,18 @@
 import * as React from 'react';
-import {Box, Container} from '@mui/material';
+import {Box} from '@mui/material';
 import WorkSpaceHeader from './WorkspaceHeader';
 import WorkspaceMyboards from './WorkspaceMyboards';
 
 
 export default function SimpleContainer() {
   
-  return (      
+  return (         
         <Box sx={{ bgcolor: '#cfe8fc', height: '90vh'}}>            
             {/* The component WorkSpace use "name props" to get the name of the WorkSpace depending on the login or name it has.
             At the momment, it has the value of "Nombre Provisional". 
             In the future will be developed the way it gets the value for "name". */}
             <WorkSpaceHeader/> {/* It has a prop "title" to change the title of this component */}  
             <WorkspaceMyboards/> {/* It has a prop "title" to change the title of this component */}                        
-        </Box>   
+        </Box> 
   );
 }
