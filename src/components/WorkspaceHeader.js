7a0402dcd54f06cdd64b450f65bc934b743c9a0c
fernandocieri <fernandocieri@gmail.com>
2022-01-15 13:33:59 +0100

    fixed displayName issue in WorkspaceHeader

--- src/components/WorkspaceHeader.js
+++ src/components/WorkspaceHeader.js
@@ -3,14 +3,15 @@ import Button from "@mui/material/Button";
 import { organizationsContext } from "./WorkspaceContainer";
 import { useState, useEffect, useContext } from "react";
 
-export default function WorkSpaceHeader({name = "Provisional Workspace Name"}){
-    const organizations = useContext(organizationsContext);
-    return(
+export default function WorkSpaceHeader() {
+    return (
         <div className="workSpace">
-        <div>{name}</div>
-        {/* Now, button has a prop called "onClick", as the attribute, to pass a function that will work 
+            <organizationsContext.Consumer>
+                {value => { if (value != undefined) return <h1>{value.displayName}</h1> }}
+            </organizationsContext.Consumer>
+            {/* Now, button has a prop called "onClick", as the attribute, to pass a function that will work 
         in the same way as if you call the event straigth from a button tag. */}
-        <Button variant="contained" className="editButton">Edit</Button>   
+            <Button variant="contained" className="editButton">Edit</Button>
         </div>
     );
 }
