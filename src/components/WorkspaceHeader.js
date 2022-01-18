0baaec114dddb43f4a9d39fd2a2a21e7787d9318
MarcoRooks <marcorooksp@gmail.com>
2022-01-18 21:18:58 +0100

    agarrar solo la informacion del ActivityList

--- src/components/WorkspaceHeader.js
+++ src/components/WorkspaceHeader.js
@@ -1,50 +1,4 @@
 import * as React from "react";
-<<<<<<< HEAD
-import axios from "axios";
-import Button from "@mui/material/Button";
-import { credentialsContext, organizationsContext } from "./WorkspaceContainer";
-import { useState, useEffect, useContext } from "react";
-import Input from '@mui/material/Input';
-
-export default function WorkSpaceHeader() {
-    const credentials = useContext(credentialsContext);
-    const organization = useContext(organizationsContext);
-    const [editableState, setEditableState] = useState({ currentValue: '', newValue: '', isBeingEdited: false })
-
-    async function handleSaveEdition() {
-        setEditableState({ ...editableState, currentValue: editableState.newValue, isBeingEdited: false });
-        let updateResponse = await axios.put(`https://api.trello.com/1/organizations/${organization.id}/?displayName=${editableState.newValue}&key=${credentialsContext.key}&token=${credentialsContext.token}`);
-    }
-
-    let currentRender = undefined;
-    function handleRender() {
-        if ((editableState.isBeingEdited === false)&& (organization !== undefined)) {
-            return (
-                <>
-                    <h1>{organization.displayName}</h1>
-                    <Button variant="contained" className="editButton" onClick={() => { setEditableState({ ...editableState, isBeingEdited: true }) }}>Edit</Button>
-                </>
-            )
-        } else if ((editableState.isBeingEdited === true) && (organization !== undefined)) {
-            return (
-                <>
-                    <Input onChange={event => setEditableState({ ...editableState, newValue: event.target.value })} defaultValue={editableState.currentValue} />
-                    <Button variant="contained" className="editButton" onClick={handleSaveEdition}>Save</Button>
-                </>
-            )
-        }
-    }
-
-    return (
-        <div className="workSpace">
-            {handleRender()}
-            {/* Now, button has a prop called "onClick", as the attribute, to pass a function that will work 
-        in the same way as if you call the event straigth from a button tag. */}
-
-        </div>
-    );
-}
-=======
 import { organizationsContext } from "./WorkspaceContainer";
 import { useState, useEffect, useContext } from "react";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
@@ -74,4 +28,3 @@ export default function WorkSpaceHeader({
     </div>
   );
 }
->>>>>>> a46e9e4619415e2a5f30d0c45e5c41a1b6ec55c3
--- src/components/hooks/useActions.js
+++ src/components/hooks/useActions.js
@@ -1,8 +1,4 @@
 import { useState } from 'react';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> d39a249234130920bbef2a657895be3088f158c3
 export default function UseActions() {
     const [open, setOpen] = useState(false);
     const [selectedValue, setSelectedValue] = useState("");
@@ -17,23 +13,4 @@ export default function UseActions() {
         { console.log(selectedValue) }
     };
     return { open, selectedValue, handleClose, handleClickOpen };
-<<<<<<< HEAD
-=======
-export default function UseActions(){
-    const [open, setOpen] = useState(false);
-    const [selectedValue, setSelectedValue] = useState("");
-  
-    const handleClickOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = (value) => {
-      setOpen(false);
-      setSelectedValue(value);
-      {console.log(selectedValue)}
-    };
-    return{open, selectedValue, handleClose, handleClickOpen};
->>>>>>> a46e9e4619415e2a5f30d0c45e5c41a1b6ec55c3
-=======
->>>>>>> d39a249234130920bbef2a657895be3088f158c3
 }
