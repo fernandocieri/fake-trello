bc9f6bb1acf19c80871d7bee061f817ab396f702
fernandocieri <fernandocieri@gmail.com>
2022-01-21 21:41:47 +0100

    installed and imported react color library

--- src/components/BoardPreview.js
+++ src/components/BoardPreview.js
@@ -12,13 +12,14 @@ import { useState, useEffect, useContext } from "react";
 import { credentialsContext } from "./WorkspaceContainer";
 import { blueGrey } from '@mui/material/colors';
 
+import { SwatchesPicker } from 'react-color';
+
 export default function BoardPreview(props) {
     const credentials = useContext(credentialsContext);
     const [boardData, setBoardData] = useState({ ...props.data });
 
-    console.log(boardData.prefs.backgroundTopColor);
     return (
-        <Card sx={{ maxWidth: 300, maxHeight: 150, backgroundColor: boardData.prefs.backgroundBottomColor, color: '292929', borderRadius: 4 }}
+        <Card sx={{ maxWidth: 300, maxHeight: 150, backgroundColor: boardData.prefs.backgroundBottomColor, color: '#292929', borderRadius: 2.5 }}
         //onClick={ }
         >
             <CardActionArea>
