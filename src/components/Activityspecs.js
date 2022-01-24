0329bf6487bcc122718db7c9837fa4dc2af1a476
fernandocieri <fernandocieri@gmail.com>
2022-01-24 14:29:52 +0100

    working on saving the value from the Inputs

--- src/components/Activityspecs.js
+++ src/components/Activityspecs.js
@@ -8,7 +8,8 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import AssignmentIcon from '@mui/icons-material/Assignment';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Grid'
+import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import { credentialsContext, getApiData } from "./WorkspaceContainer";
 
@@ -16,16 +17,27 @@ export default function Activityspecs(props) {
   const credentials = useContext(credentialsContext)
   const [cardData, setCardData] = useState({ ...props.data });
   const [cardList, setCardList] = useState()
+  const [newName, setNewName] = useState('');
+  const [newDescription, setNewDescription] = useState('')
+
   useEffect(() => {
     getApiData(setCardList, `https://api.trello.com/1/cards/${cardData.id}/list?key=${credentials.key}&token=${credentials.token}`)
   }, [])
 
-  function handleRender() {
+  function handleCardListRender() {
     if (cardList !== undefined) {
-      return cardList.name
+      return cardList.name;
     }
   }
-  
+
+  async function handleSaveEditing() {
+    //there has to be a better approach 
+    if ((newName !== '') || (newDescription !== '')) {
+      const updateResponse = await axios.put(
+        `https://api.trello.com/1/cards/${cardData.id}/?name=${newName}&desc=${newDescription}&key=${credentials.key}&token=${credentials.token}`);
+    }
+  }
+
   return (
     <Box
       component="form"
@@ -36,15 +48,14 @@ export default function Activityspecs(props) {
       autoComplete="on"
     >
       <Stack className="left-flex-container">
-        <TextField id="name" label="name" variant="outlined" defaultValue={cardData.name} />
-        <TextField id="description" label="description" variant="outlined" multiline rows={5} defaultValue={cardData.desc} />
-        <TextField id="comments" label="Comments" variant="outlined" />
+        <TextField id="name" label="name" variant="outlined" multiline maxRows={3} defaultValue={cardData.name} onChange={(event) => { setNewName(event.target.value) }} />
+        <TextField id="description" label="description" variant="outlined" multiline rows={5} defaultValue={cardData.desc} onChange={(event) => { setNewDescription(event.target.value) }} />
       </Stack>
 
       <div className="extra-info">
         <Grid item xs={3}>
           <AssignmentIcon fontSize="small" sx={{ color: '#1A5F7A' }} />
-          {handleRender()}
+          {handleCardListRender()}
         </Grid>
         <Grid item xs={3}>
           <AccessTimeFilledIcon fontSize="small" sx={{ color: '#1A5F7A' }} />
@@ -54,7 +65,7 @@ export default function Activityspecs(props) {
 
       <div className='button-section'>
         <Button variant="outlined"><KeyboardReturnIcon fontSize="small" sx={{ color: '#1A5F7A' }} />Back</Button>
-        <Button variant="outlined"><DeleteIcon fontSize="small" sx={{ color: '#1A5F7A' }} />Delete</Button>
+        <Button variant="outlined"><DeleteIcon fontSize="small" sx={{ color: '#1A5F7A' }} />Delete Card</Button>
         <Button variant="outlined"><CheckCircleIcon fontSize='small' sx={{ color: '#1A5F7A' }} />Save</Button>
       </div>
     </Box >
